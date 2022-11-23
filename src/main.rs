04c49c2cbcd2d71d250849ec6d636735cdd0ad13
ShiatsuY <dscherrmann@gmail.com>
2022-11-23 15:42:00 +0100

    implement orbs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{
     prelude::*,
     sprite::MaterialMesh2dBundle
 };
+use rand::Rng;
 
 
 
@@ -18,13 +19,13 @@ fn main() {
             ..default()
         }))
         .insert_resource(Size{
-            player: 15.,
-            star: 5.,
+            player: 0.,
+            star: 0.,
             pickup: 0.,
             orb: 0.,
         })
         .insert_resource(Speed{
-            player: 300.,
+            player: 0.,
             star: 0.,
             orb: 0.,
         })
@@ -32,12 +33,15 @@ fn main() {
         .add_system(toggle_cursor)
         .add_system(bevy::window::close_on_esc)
         .add_system(movement)
+        .add_system(orb_movement)
         .add_system(increase_size)
         .run();
 }
 
 #[derive(Component)]
 struct Player;
+#[derive(Component)]
+struct Orb;
 #[derive(Resource)]
 struct Size {
     player: f32,
@@ -57,23 +61,32 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
-    size: Res<Size>,
+    mut size: ResMut<Size>,
+    mut speed: ResMut<Speed>,
 ) {
     windows.primary_mut().set_cursor_visibility(false);
+    
     commands.spawn(Camera2dBundle::default());
+
     let window = windows.get_primary_mut().unwrap();
 
-    let p_x = -window.width()/4.;
-    let p_y = 0.;
+    // Orbs
+    size.orb = window.width() * 0.1;
+    speed.orb = 500.; // window.width() / 512.;
 
-    // Player
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(size.player).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::YELLOW)),
-        transform: Transform::from_translation(Vec3::new(p_x, p_y, 0.)),
-        ..default()
-    })
-        .insert(Player);
+    for i in 0..4{
+        let mut rng = rand::thread_rng();
+
+        let x = (window.width() + size.orb + i as f32 * size.orb * 2. + i as f32 * size.orb) - window.width()/2.;
+        let y = rng.gen_range(size.orb - window.height()/2. .. -size.orb + window.height()/2.);
+
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(size.orb).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::BLACK)),
+            transform: Transform::from_translation(Vec3::new(x, y, 0.)),
+            ..default()
+        }).insert(Orb);
+    }
 
     // Stars
     commands.spawn(MaterialMesh2dBundle {
@@ -82,6 +95,18 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
         ..default()
     });
+
+    // Player (should be drawn at the end)
+    let p_x = -window.width()/4.;
+    let p_y = 0.;
+    size.player = window.width()/80.;
+    speed.player = window.width()/4.;
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(size.player).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::YELLOW)),
+        transform: Transform::from_translation(Vec3::new(p_x, p_y, 0.)),
+        ..default()
+    }).insert(Player);
 
 }
 
@@ -94,14 +119,17 @@ fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
 
 fn increase_size(
     input: Res<Input<KeyCode>>,
+    windows: Res<Windows>,
     mut size: ResMut<Size>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
     mut query: Query<Entity, With<Player>>,
 ) {
+    let window = windows.get_primary().unwrap();
+
     if let Ok(entity) = query.get_single_mut() {
         if input.just_pressed(KeyCode::Space) {
-            size.player = size.player + 15.;
+            size.player = size.player + window.height()/4800.;
             commands
                 .entity(entity)
                 .insert(Into::<bevy::sprite::Mesh2dHandle>::into(meshes
@@ -136,5 +164,27 @@ fn movement(
             direction.x -= 1.;
         }
         transform.translation += speed.player * time.delta_seconds() * direction.normalize_or_zero();
+    }
+}
+
+fn orb_movement(
+    time: Res<Time>,
+    speed: Res<Speed>,
+    size: Res<Size>,
+    windows: Res<Windows>,
+    mut query: Query<&mut Transform, With<Orb>>,
+) {
+    let mut direction = Vec3::ZERO;
+    let mut rng = rand::thread_rng();
+    let window = windows.get_primary().unwrap();
+    direction.x = -1.;
+
+    for mut transform in query.iter_mut(){
+        if transform.translation.x <= -window.width()/2. - size.orb{
+            transform.translation.x = window.width()/2. + size.orb;
+            transform.translation.y = rng.gen_range(size.orb - window.height()/2. .. -size.orb + window.height()/2.);
+        } else {
+            transform.translation += speed.orb * time.delta_seconds() * direction.normalize_or_zero();
+        }   
     }
 }
