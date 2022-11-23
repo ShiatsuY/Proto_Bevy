2589a7668313ab34c02e03ee2657cb2d727a614e
ShiatsuY <dscherrmann@gmail.com>
2022-11-23 14:21:27 +0100

    resources

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,17 @@ fn main() {
             },
             ..default()
         }))
+        .insert_resource(Size{
+            player: 15.,
+            star: 5.,
+            pickup: 0.,
+            orb: 0.,
+        })
+        .insert_resource(Speed{
+            player: 300.,
+            star: 0.,
+            orb: 0.,
+        })
         .add_startup_system(setup)
         .add_system(toggle_cursor)
         .add_system(bevy::window::close_on_esc)
@@ -26,36 +37,47 @@ fn main() {
 }
 
 #[derive(Component)]
-struct Player(f32, f32);
+struct Player;
+#[derive(Resource)]
+struct Size {
+    player: f32,
+    star: f32,
+    pickup: f32,
+    orb: f32,
+}
+#[derive(Resource)]
+struct Speed {
+    player: f32,
+    star: f32,
+    orb: f32,
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
+    size: Res<Size>,
 ) {
     windows.primary_mut().set_cursor_visibility(false);
     commands.spawn(Camera2dBundle::default());
     let window = windows.get_primary_mut().unwrap();
-    let p_size = 15.;
-    let p_speed = 300.;
+
     let p_x = -window.width()/4.;
     let p_y = 0.;
 
-    let star_size = 5.;
-
     // Player
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(p_size).into()).into(),
+        mesh: meshes.add(shape::Circle::new(size.player).into()).into(),
         material: materials.add(ColorMaterial::from(Color::YELLOW)),
         transform: Transform::from_translation(Vec3::new(p_x, p_y, 0.)),
         ..default()
     })
-        .insert(Player(p_size, p_speed));
+        .insert(Player);
 
     // Stars
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(star_size).into()).into(),
+        mesh: meshes.add(shape::Circle::new(size.star).into()).into(),
         material: materials.add(ColorMaterial::from(Color::WHITE)),
         transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
         ..default()
@@ -72,14 +94,15 @@ fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
 
 fn increase_size(
     input: Res<Input<KeyCode>>,
+    mut size: ResMut<Size>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
-    mut query: Query<(Entity, &mut Player)>,
+    mut query: Query<Entity, With<Player>>,
 ) {
-    if let Ok((entity, mut player)) = query.get_single_mut() {
+    if let Ok(entity) = query.get_single_mut() {
         if input.just_pressed(KeyCode::Space) {
-            let tmp_size = player.0 + 50.;
-            player.0 = tmp_size;
+            let tmp_size = size.player + 50.;
+            size.player = tmp_size;
             commands
                 .entity(entity)
                 .insert(Into::<bevy::sprite::Mesh2dHandle>::into(meshes
@@ -92,14 +115,16 @@ fn increase_size(
 fn movement(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
+    size: Res<Size>,
+    speed: Res<Speed>,
     mut windows: ResMut<Windows>,
-    mut query: Query<(&mut Transform, &Player)>,
+    mut query: Query<&mut Transform, With<Player>>,
 ) {
-    if let Ok((mut transform, player)) = query.get_single_mut() {
+    if let Ok(mut transform) = query.get_single_mut() {
         let window = windows.get_primary_mut().unwrap();
         let mut direction = Vec3::ZERO;
-        let size = player.0;
-        let speed = player.1;
+        let size = size.player;
+        let speed = speed.player;
 
         if input.pressed(KeyCode::W) && transform.translation.y + size < window.height()/2.  {
             direction.y += 1.;
