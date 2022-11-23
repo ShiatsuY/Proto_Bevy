f28808c543ea5c85057a6a7046916475a9ff9f11
Yannik Haller <yannik.haller@googlemail.com>
2022-11-23 12:57:25 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,7 @@ fn main() {
 }
 
 #[derive(Component)]
-struct Player(f32, f32, f32, f32);
-
-#[derive(Component)]
-struct Movable;
+struct Player(f32, f32);
 
 fn setup(
     mut commands: Commands,
@@ -46,8 +43,6 @@ fn setup(
     let p_y = 0.;
 
     let star_size = 5.;
-    
-    commands.spawn(Player(p_size, p_speed, p_x, p_y));
 
     // Player
     commands.spawn(MaterialMesh2dBundle {
@@ -56,7 +51,7 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(p_x, p_y, 0.)),
         ..default()
     })
-    .insert(Movable);
+        .insert(Player(p_size, p_speed));
 
     // Stars
     commands.spawn(MaterialMesh2dBundle {
@@ -78,39 +73,19 @@ fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
 fn increase_size(
     input: Res<Input<KeyCode>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
-    mut player_query: Query<&mut Player>,
-    mut mesh_query: Query<Entity, With<Movable>>,
-    transform_query: Query<&Transform, With<Movable>>,
+    mut query: Query<(Entity, &mut Player)>,
 ) {
-    if input.just_pressed(KeyCode::Space) {
-        let mut p = player_query.single_mut();
-        let m = mesh_query.single_mut();
-        let transform = transform_query.single();
-        p.0 += 50.;
-        p.2 = transform.translation.x;
-        p.3 = transform.translation.y;
-
-
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::new(p.0 * 3.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::YELLOW)),
-            transform: Transform::from_translation(Vec3::new(p.2, p.3, 0.)),
-            ..default()
-        })
-        .insert(Movable);
-
-        commands.entity(m).despawn_recursive();
-
-        //let size = player.0;
-
-        //let mesh_2d = pos_query.single_mut();
-        //let mesh_handle = &mesh_2d.0;
-
-        // look for query bundles
-
-
+    if let Ok((entity, mut player)) = query.get_single_mut() {
+        if input.just_pressed(KeyCode::Space) {
+            let tmp_size = player.0 + 50.;
+            player.0 = tmp_size;
+            commands
+                .entity(entity)
+                .insert(Into::<bevy::sprite::Mesh2dHandle>::into(meshes
+                    .add(shape::Circle::new(tmp_size * 3.)
+                        .into())));
+        }
     }
 }
 
@@ -118,29 +93,26 @@ fn movement(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
     mut windows: ResMut<Windows>,
-    mut query: Query<&mut Transform, With<Movable>>,
-    mut player_query: Query<&Player>,
+    mut query: Query<(&mut Transform, &Player)>,
 ) {
-    let player = player_query.single_mut();
-    let size = player.0;
-    let speed = player.1;
-    let mut transform = query.single_mut();
-    let mut direction = Vec3::ZERO;
-    let window = windows.get_primary_mut().unwrap();
+    if let Ok((mut transform, player)) = query.get_single_mut() {
+        let window = windows.get_primary_mut().unwrap();
+        let mut direction = Vec3::ZERO;
+        let size = player.0;
+        let speed = player.1;
 
-    //println!("{},{}", player.0, player.1);
-
-    if input.pressed(KeyCode::W) && transform.translation.y + size < window.height()/2.  {
-        direction.y += 1.;
+        if input.pressed(KeyCode::W) && transform.translation.y + size < window.height()/2.  {
+            direction.y += 1.;
+        }
+        if input.pressed(KeyCode::S) && transform.translation.y - size > -window.height()/2.  {
+            direction.y -= 1.;
+        }
+        if input.pressed(KeyCode::D) && transform.translation.x + size < window.width()/2.  {
+            direction.x += 1.;
+        }
+        if input.pressed(KeyCode::A) && transform.translation.x - size > -window.width()/2. {
+            direction.x -= 1.;
+        }
+        transform.translation += speed * time.delta_seconds() * direction.normalize_or_zero();
     }
-    if input.pressed(KeyCode::S) && transform.translation.y - size > -window.height()/2.  {
-        direction.y -= 1.;
-    }
-    if input.pressed(KeyCode::D) && transform.translation.x + size < window.width()/2.  {
-        direction.x += 1.;
-    }
-    if input.pressed(KeyCode::A) && transform.translation.x - size > -window.width()/2. {
-        direction.x -= 1.; 
-    }
-    transform.translation += speed * time.delta_seconds() * direction.normalize_or_zero();
 }
